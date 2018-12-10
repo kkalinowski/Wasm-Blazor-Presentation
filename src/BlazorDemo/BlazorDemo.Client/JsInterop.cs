14097f4ce182150db6f41a7f11ea0ab741b63348
Krzysztof Kalinowski <kkalinowski@outlook.com>
2018-12-10 16:51:17 +0100

    Added js interop

---
+++ src/BlazorDemo/BlazorDemo.Client/JsInterop.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace BlazorDemo.Client
+{
+    public static class JsInterop
+    {
+        public static Task Log(string message)
+        {
+            return JSRuntime.Current.InvokeAsync<string>("console.log", message);
+        }
+    }
+}
